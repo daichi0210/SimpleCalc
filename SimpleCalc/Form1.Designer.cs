3d45ec189addecde7de4a93eb61fa74d5078e407
daichi <kabata0210@icloud.com>
2024-12-28 16:27:07 +0900

    画面設計

--- SimpleCalc/Form1.Designer.cs
+++ SimpleCalc/Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            input1TextBox = new TextBox();
+            plusLabel = new Label();
+            input2TextBox = new TextBox();
+            equalLabel = new Label();
+            answerTextBox = new TextBox();
+            CalcButton = new Button();
+            SuspendLayout();
+            // 
+            // input1TextBox
+            // 
+            input1TextBox.Location = new Point(12, 12);
+            input1TextBox.Name = "input1TextBox";
+            input1TextBox.Size = new Size(100, 23);
+            input1TextBox.TabIndex = 0;
+            // 
+            // plusLabel
+            // 
+            plusLabel.AutoSize = true;
+            plusLabel.Location = new Point(118, 15);
+            plusLabel.Name = "plusLabel";
+            plusLabel.Size = new Size(15, 15);
+            plusLabel.TabIndex = 1;
+            plusLabel.Text = "+";
+            // 
+            // input2TextBox
+            // 
+            input2TextBox.Location = new Point(139, 12);
+            input2TextBox.Name = "input2TextBox";
+            input2TextBox.Size = new Size(100, 23);
+            input2TextBox.TabIndex = 2;
+            // 
+            // equalLabel
+            // 
+            equalLabel.AutoSize = true;
+            equalLabel.Location = new Point(245, 15);
+            equalLabel.Name = "equalLabel";
+            equalLabel.Size = new Size(15, 15);
+            equalLabel.TabIndex = 3;
+            equalLabel.Text = "=";
+            // 
+            // answerTextBox
+            // 
+            answerTextBox.Location = new Point(266, 12);
+            answerTextBox.Name = "answerTextBox";
+            answerTextBox.Size = new Size(100, 23);
+            answerTextBox.TabIndex = 4;
+            // 
+            // CalcButton
+            // 
+            CalcButton.Location = new Point(12, 41);
+            CalcButton.Name = "CalcButton";
+            CalcButton.Size = new Size(354, 23);
+            CalcButton.TabIndex = 5;
+            CalcButton.Text = "button1";
+            CalcButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 71);
+            Controls.Add(CalcButton);
+            Controls.Add(answerTextBox);
+            Controls.Add(equalLabel);
+            Controls.Add(input2TextBox);
+            Controls.Add(plusLabel);
+            Controls.Add(input1TextBox);
+            Name = "Form1";
+            Text = "簡単計算プログラム";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox input1TextBox;
+        private Label plusLabel;
+        private TextBox input2TextBox;
+        private Label equalLabel;
+        private TextBox answerTextBox;
+        private Button CalcButton;
     }
 }
--- SimpleCalc/obj/Debug/net6.0-windows/SimpleCalc.AssemblyInfo.cs
+++ SimpleCalc/obj/Debug/net6.0-windows/SimpleCalc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1dd3192faecd6fee0e521fcac4d23234a9c90ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
